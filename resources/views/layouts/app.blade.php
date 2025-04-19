b72a66f8419dabfc3ca04017c0e88aceeb8376fa
desy <desii.ayurianti@gmail.com>
2025-04-19 19:27:38 +0700

    feat(favicon): adding logo for favicon & delete unuse img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
 
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('/images/logo.png') }}" type="image/png">
+
+
     <!-- Tambahkan preconnect di sini -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
