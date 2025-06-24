4c661e26ae8eeecc3e5152836f6cb8fdff9a860e
desy <desii.ayurianti@gmail.com>
2025-06-24 09:23:18 +0700

    fix: fix bug detail porto

--- resources/views/pages/detail_porto_proyek.blade.php
+++ resources/views/pages/detail_porto_proyek.blade.php
@@ -38,13 +38,13 @@
                 </div>
 
                 <div class="col-lg-8">
-                    <h3 class="mb-3 fw-bold">{{ $proyek->nama_proyek }}</h3>
-                    <p class="text-muted mb-4">{{ $proyek->deskripsi_luaran }}</p>
+                    <h3 class="mb-3 fw-bold" style="font-size: 18px;">{{ $proyek->nama_proyek }}</h3>
+                    <p class="text-muted mb-4" style="font-size: 16px;">{{ $proyek->deskripsi_luaran }}</p>
 
                     <div class="row">
                         <div class="col-md-3 mb-3">
                             <h6>Mitra Industri</h6>
-                            <div class="text-muted mb-4">
+                            <div class="text-muted mb-4" style="font-size: 14px;">
                                 @if ($proyek->nama_mitra)
                                     {{ $proyek->nama_mitra }}
                                 @else
@@ -54,7 +54,7 @@
                         </div>
                         <div class="col-md-3 mb-3">
                             <h6>Dosen</h6>
-                            <div class="text-muted mb-4">
+                            <div class="text-muted mb-4" style="font-size: 14px;">
                                 @if ($dosen->count() > 0)
                                     @foreach ($dosen as $ds)
                                         <div class="mb-2">{{$ds->nama_dosen}}</div>
@@ -66,7 +66,7 @@
                         </div>
                         <div class="col-md-3 mb-3">
                             <h6>Mahasiswa</h6>
-                            <div class="text-muted mb-4">
+                            <div class="text-muted mb-4" style="font-size: 14px;">
                                 @if ($mahasiswa->count() > 0)
                                     @foreach ($mahasiswa as $mhs)
                                         <div class="mb-2">{{$mhs->nama_mahasiswa}}</div>
@@ -78,7 +78,7 @@
                         </div>
                         <div class="col-md-3 mb-3">
                             <h6>Profesional</h6>
-                            <div class="text-muted mb-4">
+                            <div class="text-muted mb-4" style="font-size: 14px;">
                                 @if ($profesional->count() > 0)
                                     @foreach ($profesional as $prof)
                                         <div class="mb-2">{{$prof->nama_profesional}}</div>
